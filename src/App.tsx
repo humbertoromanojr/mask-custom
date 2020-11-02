e39a3511460e576b33fddf0d2a0f523b36b99cb2
junior <astronomi@gmail.com>
2020-11-02 17:20:19 -0300

    Input maskCurrency

--- src/App.tsx
+++ src/App.tsx
@@ -7,19 +7,35 @@ import Input from './components/Input';
 
 const App = () => {
   const [cep, setCep] = useState('');
-
-  const handleInputCustom = (value: string) => {
-    console.log(value);
-  };
+  const [phone, setPhone] = useState('');
+  const [currency, setCurrency] = useState('');
 
   return (
     <View style={{ flex: 1, backgroundColor: '#0c0577', alignItems: 'center', justifyContent: 'center'}}>
       <StatusBar barStyle="light-content" backgroundColor="#0c0577" />
 
       <Input
-      mask="cep"
-      value={cep}
-      inputMaskChange={(text: string) => handleInputCustom(text)} />
+        mask="cep"
+        maxLength={9}
+        value={cep}
+        placeholder="Digite o CEP"
+        inputMaskChange={(text: string) => setCep(text)}
+      />
+
+      <Input
+        mask="phone"
+        maxLength={14}
+        value={phone}
+        placeholder="Digite o Telefone"
+        inputMaskChange={(text: string) => setPhone(text)}
+      />
+
+      <Input
+        mask="currency"
+        value={currency}
+        placeholder="Digite o Valor"
+        inputMaskChange={(text: string) => setCurrency(text)}
+      />
 
     </View>
   );
