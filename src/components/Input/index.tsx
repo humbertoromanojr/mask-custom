84848ca7a9a19f02743708a0e0d695714ca0d202
junior <astronomi@gmail.com>
2020-11-02 16:08:47 -0300

    Create component input

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,43 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable prettier/prettier */
+import React from 'react';
+import { StyleSheet, TextInput } from 'react-native';
+
+import { maskCep } from '../utils/mask';
+
+interface InputProps {
+  mask: 'cep' | 'prone' | 'currency';
+  inputMaskChange: any;
+}
+
+const Input = ({ mask, inputMaskChange, ...rest }: InputProps) => {
+
+  const handleChange = (text: string) => {
+    const value = maskCep(text);
+    inputMaskChange(value);
+  };
+
+  return (
+    <>
+      <TextInput
+      style={styles.input}
+      onChangeText={text => handleChange(text)}
+      { ...rest }
+      />
+    </>
+  );
+};
+
+export default Input;
+
+const styles = StyleSheet.create({
+  input: {
+    backgroundColor: '#ffffff',
+    height: 50,
+    width: 200,
+    borderRadius: 26,
+    paddingHorizontal: 20,
+    marginTop: 10,
+    fontSize: 16,
+  },
+});
