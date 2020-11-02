97cb9413a2fc4076592ba07015973c6e878838a6
junior <astronomi@gmail.com>
2020-11-02 16:47:58 -0300

    Create maskPhone

--- src/components/utils/mask.ts
+++ src/components/utils/mask.ts
@@ -1,5 +1,13 @@
 export const maskCep = (value: string) => {
-  value = value.replace(/\D/g, '');
+  value = value.replace(/\D/g, ''); // 123456789
   value = value.replace(/^(d{5})(\d)/, '$1-$2');
+  return value;
+};
+
+export const maskPhone = (value: string) => {
+  value = value.replace(/\D/g, ''); // 123456789
+  // (99)9999-9999
+  value = value.replace(/^(d{2})(\d)/g, '($1)$2'); // (99)9999
+  value = value.replace(/(\d)(\d{4})$/, '$1-$2'); //-9999
   return value;
 };
