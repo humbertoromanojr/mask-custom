b9e6166235e71507c58a2b6d5040f20dd5b7e6df
junior <astronomi@gmail.com>
2020-11-02 17:19:09 -0300

    Create maskCurrency

--- src/components/utils/mask.ts
+++ src/components/utils/mask.ts
@@ -1,13 +1,20 @@
 export const maskCep = (value: string) => {
   value = value.replace(/\D/g, ''); // 123456789
-  value = value.replace(/^(d{5})(\d)/, '$1-$2');
+  value = value.replace(/^(\d{5})(\d)/, '$1-$2');
   return value;
 };
 
 export const maskPhone = (value: string) => {
   value = value.replace(/\D/g, ''); // 123456789
   // (99)9999-9999
-  value = value.replace(/^(d{2})(\d)/g, '($1)$2'); // (99)9999
+  value = value.replace(/^(\d{2})(\d)/g, '($1)$2'); // (99)9999
   value = value.replace(/(\d)(\d{4})$/, '$1-$2'); //-9999
+  return value;
+};
+
+export const maskCurrency = (value: string) => {
+  value = value.replace(/\D/g, '');
+  value = value.replace(/(\d)(\d{2})$/, '$1,$2');
+  value = value.replace(/(?=(\d{3})+(\D))\B/g, '.');
   return value;
 };
