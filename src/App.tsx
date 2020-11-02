6664d5981ab419cc876fabba1a4d47f685e22e31
junior <astronomi@gmail.com>
2020-11-02 15:11:55 -0300

    Changing entry point

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+// import { Container } from './styles';
+
+const App = () => {
+  return (
+    <View>
+      <Text>App screen</Text>
+    </View>
+  );
+};
+
+export default App;
